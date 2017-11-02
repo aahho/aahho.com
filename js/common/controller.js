7c0f2d18d47c87003378302a22fdc234ae004629
Himan Reddy <himan@sourceeasy.com>
2017-11-02 18:38:40 +0530

    viewable on mobile

--- js/common/controller.js
+++ js/common/controller.js
@@ -2,19 +2,17 @@
 	'use strict';
 
 	var aahhoControllers = angular.module('ngSeedApp.controllers');
-
 	
 	aahhoControllers.controller('homeController', [
-			'$scope',
-			function($scope) {
-				$scope.title = "Welcome";
-			}
+		'$scope', 
+		function($scope) {
+			$scope.title = "Welcome";		
+		}
 	]);
 
 	aahhoControllers.controller('workController', [
-		'$scope',
+		'$scope', 
 		function($scope) {
-
 			$scope.slides = [
 				{name: 'Wedding Site for US', url: './img/work/3knots.jpg'},
 				{name: 'Corporate Game for Boston Consulting Group (BCG)', url: './img/work/AnurakshanBCG.png'},
@@ -23,61 +21,53 @@
 				{name: 'Mobile community', url: './img/work/MobilityNinja.png'},
 				{name: 'Prototyping productivity', url: './img/work/nevermissit.png'},
 				{name: 'Mobile device UI', url: './img/work/seagate.png'},
-				{name: 'Conference landing page', url: './img/work/wwb.png'},
-				
-
-				
-			]
-
-
-
+				{name: 'Conference landing page', url: './img/work/wwb.png'}
+			];
 			$scope.title = "Welcome";
 		}
-]);
+	]);
 
-aahhoControllers.controller('servicesController', [
-	'$scope',
-	function($scope) {
-		$scope.title = "Welcome";
-	}
-]);
-aahhoControllers.controller('productsController', [
-	'$scope',
-	function($scope) {
-		$scope.title = "Welcome";
-	}
-]);
-aahhoControllers.controller('aboutController', [
-	'$scope',
-	function($scope) {
-		$scope.title = "Welcome";
-	}
-]);
-
-aahhoControllers.controller('contactController', [
-	'$scope','$firebaseArray',
-	function($scope, $firebaseArray) {
-		$scope.title = "Welcome";
-
-
-		$scope.submitContact = function(contact) {
-			var database = firebase.database();
-    		var ref = firebase.database().ref('/contacts');
-     		var object =  {
-            'contactName' : contact.name,
-            'contactEmail' : contact.email,
-            'contactPhone' : contact.phone
-        }
-
-			 const promise = ref.push(object);
-			  promise.catch(e => console.log(e.message));
-          	  promise.then(function() {
-                console.log("done");
-                
-            })
+	aahhoControllers.controller('servicesController', [
+		'$scope',
+		function($scope) {
+			$scope.title = "Welcome";
 		}
-	}
-]);
+	]);
+	aahhoControllers.controller('productsController', [
+		'$scope',
+		function($scope) {
+			$scope.title = "Welcome";
+		}
+	]);
+	aahhoControllers.controller('aboutController', [
+		'$scope',
+		function($scope) {
+			$scope.title = "Welcome";
+		}
+	]);
+
+	aahhoControllers.controller('contactController', [
+		'$scope','$firebaseArray',
+		function($scope, $firebaseArray) {
+			$scope.title = "Welcome";
+
+			$scope.submitContact = function(contact) {
+				var database = firebase.database();
+				var ref = firebase.database().ref('/contacts');
+				var object =  {
+					'contactName' : contact.name,
+					'contactEmail' : contact.email,
+					'contactPhone' : contact.phone
+				}
+
+				const promise = ref.push(object);
+				promise.catch(e => console.log(e.message));
+				promise.then(function() {
+					console.log("done");
+				})
+			}
+		}
+	]);
 
 
 }());
--- main.js
+++ main.js
@@ -10,18 +10,7 @@
 			'firebase'
 		]);
 
-	aahhoWeb.run(['$rootScope', '$state', '$stateParams',
-			function($rootScope, $state, $stateParams) {
-
-				//Add references to $state and $stateParams to the $rootScope
-				$rootScope.$state = $state;
-				$rootScope.$stateParams = $stateParams;
-
-				$rootScope.$on('$routeChangeSuccess', function (event, current, previous) {
-					$rootScope.title = current.$$route.title;
-				});
-			}
-		])
+	aahhoWeb
 		.config(['$stateProvider', '$urlRouterProvider',
 			function($stateProvider, $urlRouterProvider) {
 
@@ -59,6 +48,53 @@
 					controller: 'contactController',
 					templateUrl: 'views/contact.html'
 				});
+			}
+		])
+		.run(['$rootScope', '$state', '$stateParams',
+			function($rootScope, $state, $stateParams) {
+
+				//Add references to $state and $stateParams to the $rootScope
+				$rootScope.$state = $state;
+				$rootScope.$stateParams = $stateParams;
+
+				$rootScope.$on('$stateChangeStart', function (event) {
+					console.log('state change started');
+					var sidebar = $('.sidebar');
+					var em = sidebar.find('#sidebar-btn');
+					if(sidebar.hasClass('sidebar-sm')) {
+						sidebar.removeClass('sidebar-sm');
+						em.removeClass('fa-times');
+						em.addClass('fa-bars');
+					}
+				});
+
+				// $rootScope.$on('$stateChangeSuccess', function (event) {
+				// 	console.log('state change success');
+				// 	var sidebar = $('.sidebar');
+				// 	if(sidebar.hasClass('sidebar-sm')) {
+				// 		sidebar.removeClass('sidebar-sm');
+				// 	}
+				// });
+
+				$rootScope.$on('$routeChangeSuccess', function (event, current, previous) {
+					$rootScope.title = current.$$route.title;
+				});
+
+				$rootScope.toggleSidebar = function ($event) {
+					// console.log($event);
+					var sidebar = $('.sidebar');
+					var em = sidebar.find('#sidebar-btn');
+					console.log(em);
+					if(sidebar.hasClass('sidebar-sm')) {
+						sidebar.removeClass('sidebar-sm');
+						em.removeClass('fa-times');
+						em.addClass('fa-bars');
+					} else {
+						sidebar.addClass('sidebar-sm');
+						em.removeClass('fa-bars');
+						em.addClass('fa-times');
+					}
+				};
 			}
 		]);
 }());
